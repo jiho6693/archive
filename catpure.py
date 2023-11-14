bb2f1bb39bda8eeff83bb53604a36fde731f6d6d
jiho6693 <56946702+jiho6693@users.noreply.github.com>
2023-11-13 21:10:03 -0500

    a

--- catpure.py
+++ catpure.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 # URL of the website you want to capture
-target_url = "https://www.jiho6693.com/"
+target_url = "https://www.google.com/search?q=anthony+mccall&rlz=1C5CHFA_enKR1016KR1017&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjl5oeik5v9AhXrrokEHYDtCEcQ_AUoAXoECAEQAw&biw=1147&bih=637&dpr=2"
 
 # Send a GET request to the website
 response = requests.get(target_url)
--- screenshot.js
+++ screenshot.js
@@ -5,17 +5,17 @@ const path = require('path');
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     
-    // 웹사이트로 이동
+    // go to website
     await page.goto('https://sagmeisterwalsh.com/work/all/beauty/');
 
-    // 현재 페이지의 URL에서 파일 이름을 추출
+    // filename extract
     const pageName = page.url().replace(/[^a-zA-Z0-9]/g, '_');
 
-    // 스크린샷 찍기
+    // screenshot
     const screenshotPath = path.join(__dirname, `./images/${pageName}.jpg`);
     await page.screenshot({ path: screenshotPath });
     console.log(`Screenshot saved to: ${screenshotPath}`);
 
-    // 브라우저 종료
+    // quit borwer
     await browser.close();
 })();
